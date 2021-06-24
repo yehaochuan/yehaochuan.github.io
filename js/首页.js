c744bb369eeef2400e04169243b2a0002ede9427
yehaochuan <83534090+yehaochuan@users.noreply.github.com>
2021-06-24 23:21:34 +0800

    Update 首页.js

--- js/首页.js
+++ js/首页.js
@@ -1,65 +1,28 @@
-// 中部
-var mid=new Vue({
-    el:"#box",
-    data:{
-      
-      //头部数据
-      dh_arr:["css3动画","小游戏","canavs动画","项目",'查看更多',"CSDN博客"],
-       
-      anli:["","","","","","","","",""],
-      arr_src1:['./img/css3-1.png','./img/css3-2.png','./img/css3-3.png','./img/css3-4.png','./img/css3-5.png','./img/css3-6.png'],
-      nr:["css3-手风琴","css3-快乐星球",'css3-核反应堆','css3-Iron man','css3-血轮眼',"css3-地爆天星"],
-      href_1:["./css3/手风琴/index.html","./css3/星球/index.html","./css3/Iron man/iron.html","./css3/Iron man/iron.html","./css3/血轮眼/demo.html","./css3/地爆天星/index.html"],
-          bankuai:["",
-                  "",
-                  "",
-                  "",
-                  "",
-                
-          ],
+// 点击切换按钮
+$('.option_btn>div').on('click', event =>{
+        $('.option_btn>div').removeClass('selected');
+        $(event.target).addClass('selected');
+        time = $(event.target).index();
+        change_bg(time);
+
+})
+
+// 定时器自动切换
+    var time = 0; 
+    setInterval(() => {
+        $('.option_btn>div').removeClass('selected');
+        $('.option_btn>div').eq(time).addClass('selected');
+        change_bg(time);
+        if(time==2){
+            time=0;
+        }else{
+             time++;
+        }
+    }, 3000);
 
 
-
-          bool : true,
-        
-        },
-
-            
-
- 
-    
-    methods:{
-           dianzan:function(event){
-               var e = event.currentTarget;
-                e.src="./img/点赞 (2).png"
-                e.nextSibling.innerText++;
-           },
-           caidan:function(){
-              if(this.bool){
-                  caidan_box.style.display='block';
-              }else{
-                caidan_box.style.display='none';
-              }
-               this.bool = !this.bool;
-               
-
-           }
-
-
-    },
-  
-
-}) 
-
-
-
-
-  
-
-$(function(){
-  $('#dowebok').fullpage({
-      sectionsColor: [ 'rgba(0,0,0,0)', '#4BBFC3', '#7BAABE', '#f90']
-  });
-});
-
-
+// 绑定背景
+    function change_bg(time){
+        $('body').get(0).className = "";
+         $('body').eq(0).addClass(`bg${time+1}`)
+    }
