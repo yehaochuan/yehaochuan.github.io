70761a4f2dba5214ef28e394e059c049c8827ef2
yehaochuan <83534090+yehaochuan@users.noreply.github.com>
2021-06-30 22:23:05 +0800

    Update 首页.js

--- js/首页.js
+++ js/首页.js
@@ -18,7 +18,7 @@ $('.option_btn>div').on('click', event =>{
         }else{
              time++;
         }
-    }, 3000);
+    }, 5000);
 
 
 // 绑定背景
@@ -26,3 +26,27 @@ $('.option_btn>div').on('click', event =>{
         $('body').get(0).className = "";
          $('body').eq(0).addClass(`bg${time+1}`)
     }
+
+
+//简历按钮
+    $('.janli_button>div').on('click',(e)=>{
+        $('.janli_button>div').removeClass('selected');
+        $(e.target).addClass('selected');
+        console.log(e.target.dataset.imgnum);
+        $('.jianli').css('background-image',`   url(./img/j${e.target.dataset.imgnum}.png)`)
+    })
+
+//fullpage跳转
+    $('.jili_right').on('click',()=>{
+        console.log(1);
+        $.fn.fullpage.moveTo(1,2)
+    })
+    $('.anli').on('click',()=>{
+        $.fn.fullpage.moveTo(2)
+    })
+    
+    $('.lianxi').on('click',()=>{
+        $.fn.fullpage.moveTo(1,1)
+    })
+        
+      
